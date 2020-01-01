b94f4654d2cda9ed78673b9612a053c829a3ade7
evanandrewson <evanandrewson@gmail.com>
2020-01-01 15:29:22 -0800

    made PokemonList component

---
+++ src/components/Pokemon/PokemonList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import PokemonItem from './PokemonItem';
+
+const PokemonList = ({ list }) => {
+  const elements = list.map(element => {
+    return <PokemonItem {...element} key={element.pokemon} />;
+  });
+  return (
+    <ul>
+      {elements}
+    </ul>
+  );
+};
+
+PokemonList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    pokemon: PropTypes.string.isRequired,
+    url_image: PropTypes.string.isRequired,
+    attack: PropTypes.string.isRequired,
+    defense: PropTypes.string.isRequired,
+    type_1: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default PokemonList;
